e9a5071edc3c0ac0a8824d39703486e3d23efe23
Carl Bernardo <carlbernrdo@gmail.com>
2016-04-11 09:39:46 -0700

    [feat] display song / songlist component after fetch

--- common/components/Song.js
+++ common/components/Song.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 class Song extends React.Component{
   render(){
+    const { id } = this.props;
+
     return(
       <li>
         <iframe
           width='420'
           height='315'
-          src='http://www.youtube.com/embed/0CFuCYNx-1g'
+          src={'http://www.youtube.com/embed/' + id}
           frameBorder='0'
           allowFullScreen></iframe>
       </li>
--- common/components/Songlist.js
+++ common/components/Songlist.js
@@ -2,13 +2,42 @@ import React from 'react';
 import Song from './Song';
 
 class Songlist extends React.Component{
+  constructor(){
+    super();
+    this.displaySonglist = this.displaySonglist.bind(this);
+  }
+
+  displaySonglist(){
+    const { videos } = this.props;
+    let songs;
+
+    if(videos.length > 0){
+      songs = videos.map((video, ind) => {
+        return <Song id={video.id.videoId} key={ind} />
+      });
+
+      return songs;
+    } else {
+      return (
+        <li>
+          <iframe
+            width='420'
+            height='315'
+            src='http://www.youtube.com/embed/0CFuCYNx-1g'
+            frameBorder='0'
+            allowFullScreen></iframe>
+        </li>
+      )
+    }
+  }
+
   render(){
     console.log('props in songlist', this.props);
     return(
       <div>
-        SONGS:
+        <h2>Songs</h2>
         <ul>
-          <Song />
+          {this.displaySonglist()}
         </ul>
       </div>
     )
